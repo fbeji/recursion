24f4745480760e12bcd715b365004d3272d9451b
fbeji <fbeji@unm.edu>
2019-02-07 09:07:46 -0700

    Initial commit (recursive Fibonacci)

---
+++ src/edu/cnm/deepdive/Fibonacci.java
@@ -0,0 +1,17 @@
+package edu.cnm.deepdive;
+
+public class Fibonacci {
+
+  public static long fibonnaci(int n){
+
+    if ( n<= 0 ){
+      return 0;
+    }
+    if (n == 1) {
+      return 1;
+    }
+
+    return fibonnaci(n -1)+ fibonnaci(n-2);
+  }
+
+}
